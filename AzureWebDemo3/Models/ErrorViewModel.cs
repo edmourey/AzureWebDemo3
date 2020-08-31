ecb53478aa62be92dd44bed96cf3a01781b75749
Eduardo Mourey <MoureyEduardo@JohnDeere.com>
2020-08-31 17:40:05 -0500

    Add project files.

---
+++ AzureWebDemo3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureWebDemo3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
